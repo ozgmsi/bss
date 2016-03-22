d5d1aba4caea9e18fa1a5ba95682af518867d6e3
Tim Katgert <timkatgert@hotmail.com>
2016-03-22 14:22:33 +0100

    Superblock,  freelist en rootNode inlezen afgemaakt.

--- Opdracht 2/e7dump/main.cc
+++ Opdracht 2/e7dump/main.cc
@@ -33,17 +33,63 @@ using namespace std;
 
 void readSuperBlock(Device& device, ostream& out)
 {
-
+    Block *sb = device.getBlock(SUPERB);
+    out << "Dump of superblock on" << sb->u.fs.s_fname << "." << sb->u.fs.s_fpack << endl;
+    out << "|--------------------------------------------|" << endl;
+    out << "userdata area starts in block: " << sb->u.fs.s_isize << endl;
+    out << "number of blocks on volume is: " << sb->u.fs.s_fsize << endl;
+    out << "number of freeblocks in free[] is: " << sb->u.fs.s_nfree << " (max 50)" << endl;
+    for (int i = 0; i < sb->u.fs.s_nfree; i++){
+        out << " " << sb->u.fs.s_free[i];
+    }
+    out << endl;
+    out << "number of free inodes in inode[] is: " << sb->u.fs.s_ninode << " (max 100)" << endl;
+    for (int i = 0; i < sb->u.fs.s_ninode; i++){
+        out << " " << sb->u.fs.s_inode[i];
+    }
+    out << endl;
+    out << "freelist lock flag is: " << (sb.u.fs.s_flock ? "set" : "not set") << endl;
+    out << "i-list lock flag is: " << (sb.u.fs.s_ilock ? "set" : "not set") << endl;
+    out << "superblock modified is: " << (sb.u.fs.s_fmod ? "set" : "not set") << endl;
+    out << "read only flag is: " << (sb.u.fs.s_ronly ? "set" : "not set") << endl;
+    out << "last update time was: " << ctime(&sb->u.fs.s_time) << endl;
+    out << "total number of free block" << sb->u.fs.s_tfree << endl;
+    out << "total number of inodes" << sb->u.fs.s_tinode << endl;
+    out << "Interleave factors are: " << sb->u.fs.s_m << " n=" << sb->u.fs.s_n << endl;
+    out << "File system name" << sb->u.fs.s_fname << endl;
+    out << "File system pack" << sb->u.fs.s_fpack << endl;
+    out << "|--------------------------------------------|" << endl;
+    out << "Rest of free list continues in block " << sb->u.fs.s_free[0] << endl;
+    out << "|--------------------------------------------|" << endl;
+    readFree(device, out, sb->u.fs.free[0]);
+    sb.release();
 }
 
 void readFree(Device& device, ostream& out, daddr_x addr)
 {
-
+    if (addr == 0){
+        return;
+    }
+    Block *fl = device.getBlock(addr);
+    out << "Freeblock " << addr << ": " << fl->u.fb.df_nfree << "(max 50)" << endl;
+    for (int i = 0; i < fl->u.fb.df_nfree; i++){
+        out << " " << fl->u.fb.df_free[i];
+    }
+    out << endl;
+    readFree(device, out, fl->u.fb.df_free[0]);
+    fl->release();
 }
 
 void readRootNode(Device& device, ostream& out)
 {
-
+    Block *inode = device.getBlock(SUPERB);
+    int inodes = (inode->u->fs.s_isize-2) * INOPB;
+    out << "Reading " << inodes << "inodes" << endl;
+    out << "|--------------------------------------------|" << endl;
+    for (int i = 1; i < inodes; i++){
+        readInodes(device, out, i);
+    }
+    inode->release();
 }
 
 void readInodes(Device& device, ostream& out, ino_x addr)
